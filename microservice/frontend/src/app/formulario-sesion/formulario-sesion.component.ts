5b34e03c7124be64064f35ef5161cdcdb3cd88db
Salva <salvadorov2@gmail.com>
2024-03-27 13:29:02 +0100

    Cambios en Formulario-Sesion
    Al final estoy optando por la idea de Javi de usar FormGroups de Angular para instanciar todo el formulario. En el código he comentado lo anterior. No sé por qué el calendario y hora que desarrolló Javi ahora no funciona así que toca retocar algunas cosa

--- microservice/frontend/src/app/formulario-sesion/formulario-sesion.component.ts
+++ microservice/frontend/src/app/formulario-sesion/formulario-sesion.component.ts
@@ -13,7 +13,78 @@ import { CommonModule, DatePipe } from '@angular/common';
   providers: [DatePipe, NgbActiveModal]
 })
 export class FormularioSesionComponent {  //Reactive form
+  /***********************************DATE ATTRIBUTES***********************************/
+  private currentDate : string | null = '';
+  private currentTime : string | null = '';
+  private cTimePlusHour : string | null = '';
+  /*************************************************************************************/
+  
 
+  constructor(private fb: FormBuilder, private datePipe: DatePipe) {
+    let currentDateAndTime = new Date();
+    let tmpOneMoreHour = new Date();
+    tmpOneMoreHour.setTime(tmpOneMoreHour.getTime() + 60*60*1000);  //We add one hour to the start date
+    this.currentDate = this.datePipe.transform(currentDateAndTime, 'yyyy-MM-dd'); //Default start date value
+    this.currentTime = this.datePipe.transform(currentDateAndTime, 'HH:mm');  //Default start time value
+    this.cTimePlusHour = this.datePipe.transform(tmpOneMoreHour, 'HH:mm');  //Default end time value
+  }
+
+  /***********************************DATE FUNCTIONS***********************************/
+  private checkIfEndTimeAfterStartTime (c: AbstractControl) {
+    //Safety check
+    if (!c.get('starttime')?.value || !c.get('endtime')?.value) { return null }
+    //If the values are valid we need to return null
+    //Otherwise we will set the error flag 'invalidEndTime' as true
+    if(c.get('endtime')?.value > c.get('starttime')?.value){
+      return null;
+    }else{
+      return {invalidEndTime: true}
+    }
+  }
+
+  get startdate(){
+    return this.workoutForm.get('startdate');
+  }
+  get starttime(){
+    return this.workoutForm.get('starttime');
+  }
+  get endtime(){
+    return this.workoutForm.get('endtime');
+  }
+
+  get form(){
+    return this.workoutForm;
+  }
+  get fcontrols(){
+    return this.workoutForm.controls;
+  }
+  /*************************************************************************************/
+
+
+  workoutForm = this.fb.group({
+    startdate: [this.currentDate, Validators.required],
+    starttime: [this.currentTime, Validators.required],
+    endtime: [this.cTimePlusHour, Validators.required],
+    exercise_completed: '',
+    comments: ['', Validators.maxLength(300)],
+    isInPerson: false,
+    workoutRegister : this.fb.group({
+      beats: '',
+      weight: '',
+      burned_calories: ['', Validators.maxLength(5)],
+    }),
+    //Falta añadir video y foto. Tal vez ID?
+  }, 
+  {
+    validators: [this.checkIfEndTimeAfterStartTime] //We need to validate that the end time is later than the start time
+  });  
+}
+
+
+
+
+
+  /*
   //Attributes
   WorkoutForm : FormGroup;
 
@@ -30,6 +101,7 @@ export class FormularioSesionComponent {  //Reactive form
     photo: undefined,
     id: undefined,
   };
+
 
   //Default values
   private currentDate : string | null = '';
@@ -90,46 +162,4 @@ export class FormularioSesionComponent {  //Reactive form
   get fcontrols(){
     return this.WorkoutForm.controls;
   }
-}
-
-
-/*
-import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CommonModule, DatePipe } from '@angular/common';
-import { Workout } from '../workout';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-@Component({
-  selector: 'app-formulario-sesion',
-  standalone: true,
-  templateUrl: './formulario-sesion.component.html',
-  styleUrl: './formulario-sesion.component.css'
-})
-export class FormularioSesionComponent {
-  accion?: 'Enviar' | 'Cerrar'; // | 'Borrador' ?
-  sesion: Workout = {
-    start_date: new Date(), 
-    end_date: new Date(),
-    exercise_completed: '',
-    comments: '',
-    isInPerson: true,
-    beat: '',
-    weight: '',
-    burned_calories: '',
-    video: undefined,
-    photo: undefined,
-    id: undefined,
-  };
-
-  constructor(public modal: NgbActiveModal) {
-  }
-  
-  closeForm(): void {
-    this.modal.close(this.sesion);
-  }
-
-}
-*/
-
-/*
-*/
+  */
