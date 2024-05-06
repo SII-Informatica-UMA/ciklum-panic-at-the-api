b8388c9f2cb4262c256acebb1dca5e4c1fb3a5a7
CabezaNido <117044296+CabezaNido@users.noreply.github.com>
2024-05-06 13:03:57 +0200

    feat((URI)
    He agregado la URI y sus métodos.

--- microservice/backend/entidades-jpa/src/test/java/es/panic/sii/entidadesjpa/EntidadesYRestApplicationTests.java
+++ microservice/backend/entidades-jpa/src/test/java/es/panic/sii/entidadesjpa/EntidadesYRestApplicationTests.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+
+import java.net.URI;
 
 @SpringBootTest
 class EntidadesYRestApplicationTests {
@@ -14,6 +21,47 @@ class EntidadesYRestApplicationTests {
 	private TestRestTemplate restTemplate;
 	@Autowired
 	private SesionRepository sesionRepo;
+	private URI uri(String scheme, String host, int port, String ...paths) {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+		UriBuilder ub = ubf.builder()
+				.scheme(scheme)
+				.host(host).port(port);
+		for (String path: paths) {
+			ub = ub.path(path);
+		}
+		return ub.build();
+	}
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.get(uri)
+				.accept(MediaType.APPLICATION_JSON)
+				.build();
+		return peticion;
+	}
+
+	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.delete(uri)
+				.build();
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.post(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.put(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
 	@Test
 	void contextLoads() {
 	}
