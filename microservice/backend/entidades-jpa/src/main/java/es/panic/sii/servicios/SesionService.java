51e1d0a15828457c5b9655c150dae0485f8c6a82
anlli <40825528+anlli@users.noreply.github.com>
2024-05-21 19:23:45 +0200

    Update SesionService.java

--- microservice/backend/entidades-jpa/src/main/java/es/panic/sii/servicios/SesionService.java
+++ microservice/backend/entidades-jpa/src/main/java/es/panic/sii/servicios/SesionService.java
@@ -1,8 +1,14 @@
 package es.panic.sii.servicios;
 
 import es.panic.sii.controladores.SesionREST;
+import es.panic.sii.entidades.Sesion;
 import es.panic.sii.repositorios.SesionRepository;
+import es.panic.sii.servicios.excepciones.SesionNoExistente;
+import jakarta.persistence.EntityExistsException;
 import jakarta.transaction.Transactional;
+import es.panic.sii.entidades.Sesion;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,4 +20,28 @@ public class SesionService {
     public SesionService(SesionRepository repo) {
         this.repo = repo;
     } //ta perfe
+
+
+    public Sesion agregarSesion(Sesion s){
+        if (repo.existsById(s.getId())) {
+			repo.save(s);
+			return s;
+		} else {
+            return null;
+		}
+    }
+
+    public Sesion borrarSesion(){
+        return null;
+    }
+    public Sesion editarSesion(){
+        return null;
+    }
+    public Sesion obtenerSesionPorId(){
+        return null;
+    }
+
+    public List<Sesion> obtenerSesionPorPlan(){
+        return null;
+    }
 }
